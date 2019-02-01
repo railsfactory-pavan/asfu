f6b82c4aaeb7a38e607f35ea47a5cd55e1ccb70e
railsfactory-pavan <pavanrf07@gmail.com>
2019-02-01 14:42:35 +0530

    migration for pin

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_01_081113) do
+ActiveRecord::Schema.define(version: 2019_02_01_091127) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -36,6 +36,13 @@ ActiveRecord::Schema.define(version: 2019_02_01_081113) do
   create_table "drops", force: :cascade do |t|
     t.string "title"
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "pins", force: :cascade do |t|
+    t.string "title"
+    t.text "caption"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
